c1f47d432fa773650ddfc19f3ff7414c685d9f3b
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-10-27 15:40:36 +0200

    Add DomainExtensions tests

---
+++ TDDLab.Tests/Core/InvoiceMgmt/DomainExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using TDDLab.Core.InvoiceMgmt;
+
+namespace TDDLab.Tests.Core.InvoiceMgmt
+{
+    public class DomainExtensionsTests
+    {
+        [Fact]
+        public void Should_ReturnMoneyWithNewCurrency_When_ToConvert()
+        {
+            var money = new Mock<Money>((ulong)100);
+            const string newCurrencyName = "NewCurrency";
+
+            // ReSharper disable once InvokeAsExtensionMethod
+            var subject = DomainExtensions.ToCurrency(money.Object, newCurrencyName);
+
+            Assert.Equal(newCurrencyName, subject.Currency);
+        }
+    }
+}
