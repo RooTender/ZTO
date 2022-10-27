084daebd5976ad002cdb23b896fa0ab59bb0484c
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-10-27 14:38:54 +0200

    Cover public methods of Address class

--- TDDLab.Tests/Core/InvoiceMgmt/AddressTests.cs
+++ TDDLab.Tests/Core/InvoiceMgmt/AddressTests.cs
@@ -1,3 +1,4 @@
+using BasicUtils;
 using TDDLab.Core.InvoiceMgmt;
 
 namespace TDDLab.Tests.Core.InvoiceMgmt
@@ -43,6 +44,46 @@ namespace TDDLab.Tests.Core.InvoiceMgmt
             var zip = _subject.Zip;
 
             Assert.Equal(Zip, zip);
+        }
+
+        [Fact]
+        public void Should_ReturnProperBusinessObject_When_CallingValidationRuleAddressLine1Getter()
+        {
+            var subject = Address.ValidationRules.AddressLine1;
+
+            Assert.IsType<BusinessRule<Address>>(subject);
+            Assert.Equal("get_AddressLine1", subject.Name);
+            Assert.Equal("AddressLine1 should be specified", subject.Description);
+        }
+
+        [Fact]
+        public void Should_ReturnProperBusinessObject_When_CallingValidationRuleCityGetter()
+        {
+            var subject = Address.ValidationRules.City;
+
+            Assert.IsType<BusinessRule<Address>>(subject);
+            Assert.Equal("get_City", subject.Name);
+            Assert.Equal("City should be specified", subject.Description);
+        }
+
+        [Fact]
+        public void Should_ReturnProperBusinessObject_When_CallingValidationRuleStateGetter()
+        {
+            var subject = Address.ValidationRules.State;
+
+            Assert.IsType<BusinessRule<Address>>(subject);
+            Assert.Equal("get_State", subject.Name);
+            Assert.Equal("State should be properly specified", subject.Description);
+        }
+
+        [Fact]
+        public void Should_ReturnProperBusinessObject_When_CallingValidationRuleZipGetter()
+        {
+            var subject = Address.ValidationRules.Zip;
+
+            Assert.IsType<BusinessRule<Address>>(subject);
+            Assert.Equal("get_Zip", subject.Name);
+            Assert.Equal("Zip code should be specified", subject.Description);
         }
     }
 }
