07fdb1ae677bf45d6c7fcc1c22d8cbbb4dcdba48
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-10-28 16:30:48 +0200

    Add Recipient tests

---
+++ TDDLab.Tests/Core/InvoiceMgmt/RecipientTests.cs
@@ -0,0 +1,73 @@
+﻿using BasicUtils;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDLab.Core.InvoiceMgmt;
+
+namespace TDDLab.Tests.Core.InvoiceMgmt
+{
+    public class RecipientTests
+    {
+        private const string TestName = "TestName";
+        private static readonly Address TestAddress = new("address", "city", "state", "zip");
+
+        private readonly Recipient _subject = new(TestName, TestAddress);
+
+        [Fact]
+        public void Should_ReturnProperRule_When_ValidationRuleName()
+        {
+            var subject = Recipient.ValidationRules.Name;
+            
+            Assert.IsType<BusinessRule<Recipient>>(subject);
+            Assert.Equal("get_Name", subject.Name);
+            Assert.Equal("Recipient name should be specified", subject.Description);
+        }
+
+        [Fact]
+        public void Should_ReturnProperRule_When_ValidationRuleAddress()
+        {
+            var subject = Recipient.ValidationRules.Address;
+            
+            Assert.IsType<BusinessRule<Recipient>>(subject);
+            Assert.Equal("get_Address", subject.Name);
+            Assert.Equal("Address should be valid", subject.Description);
+        }
+
+        [Fact]
+        public void Should_ValidateWithValidData_When_Validate()
+        {
+            var result = _subject.Validate();
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Should_ReturnBrokenRule_When_NameIsNull_When_Validate()
+        {
+            var subject = new Recipient(null, TestAddress);
+
+            var result = subject.Validate();
+
+            Assert.Single(result);
+            Assert.Collection<IRule>(result, 
+                item => Assert.Equal(Recipient.ValidationRules.Name, item)
+            );
+        }
+
+        [Fact]
+        public void Should_ReturnBrokenRule_When_NameIsEmpty_When_Validate()
+        {
+            var subject = new Recipient(string.Empty, TestAddress);
+
+            var result = subject.Validate();
+
+            Assert.Single(result);
+            Assert.Collection<IRule>(result, 
+                item => Assert.Equal(Recipient.ValidationRules.Name, item)
+            );
+        }
+    }
+}
