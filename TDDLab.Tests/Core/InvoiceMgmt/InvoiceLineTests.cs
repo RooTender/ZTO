c46baf1ee80c4fb045a1440a23335f979fa5f7ee
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-10-28 15:22:24 +0200

    Add unit tests

--- TDDLab.Tests/Core/InvoiceMgmt/InvoiceLineTests.cs
+++ TDDLab.Tests/Core/InvoiceMgmt/InvoiceLineTests.cs
@@ -6,48 +6,95 @@ namespace TDDLab.Tests.Core.InvoiceMgmt
 {
     public class InvoiceLineTests
     {
-        private const string TestProductName = "TestName";
-        private const ulong TestMoneyAmount = 100;
 
+        private class MoneyStub : Money
+        {
+            public MoneyStub() : base(100, "stub")
+            {
+            }
+
+            public new static string ToString() { return "Fake Money"; }
+        }
+        
+        private const string ProductName = "TestName";
+        private readonly MoneyStub MockMoney = new();
         private readonly InvoiceLine _subject;
 
-        public InvoiceLineTests()
-        {
-            var money = new Mock<Money>(TestMoneyAmount)
-            {
-                CallBase = true
-            };
-            money.Setup(x => x.Amount).Returns(100);
-
-            _subject = new InvoiceLine(TestProductName, money.Object);
-        }
+        public InvoiceLineTests() => _subject = new InvoiceLine(ProductName, MockMoney);
 
         [Fact]
         public void Should_FormatInvoiceLineToString_When_ToString()
         {
             var result = _subject.ToString();
 
-            Assert.Equal(result, $"{TestProductName} for {TestMoneyAmount}");
+            Assert.Equal(result, $"{ProductName} for {MockMoney}");
         }
 
         [Fact]
         public void Should_ReturnProperRule_When_ValidationRuleProductName()
         {
-            var subject = Invoice.ValidationRules.Lines;
+            var subject = InvoiceLine.ValidationRules.ProductName;
             
             Assert.IsType<BusinessRule<InvoiceLine>>(subject);
-            Assert.Equal("get_Lines", subject.Name);
-            Assert.Equal("Invoice lines should all be valid", subject.Description);
+            Assert.Equal("get_ProductName", subject.Name);
+            Assert.Equal("Product name should be specified", subject.Description);
         }
 
         [Fact]
         public void Should_ReturnProperRule_When_ValidationRuleMoney()
         {
-            var subject = Invoice.ValidationRules.Lines;
+            var subject = InvoiceLine.ValidationRules.Money;
             
             Assert.IsType<BusinessRule<InvoiceLine>>(subject);
-            Assert.Equal("get_Lines", subject.Name);
-            Assert.Equal("Invoice lines should all be valid", subject.Description);
+            Assert.Equal("get_Money", subject.Name);
+            Assert.Equal("Money should be valid", subject.Description);
+        }
+
+        [Fact]
+        public void Should_ValidateWithValidData_When_Validate()
+        {
+            var result = _subject.Validate();
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Should_ReturnBrokenRule_When_ProductNameIsNull_When_Validate()
+        {
+            var subject = new InvoiceLine(null, MockMoney);
+
+            var result = subject.Validate();
+
+            Assert.Single(result);
+            Assert.Collection<IRule>(result, 
+                item => Assert.Equal(InvoiceLine.ValidationRules.ProductName, item)
+            );
+        }
+
+        [Fact]
+        public void Should_ReturnBrokenRule_When_ProductNameIsEmpty_When_Validate()
+        {
+            var subject = new InvoiceLine(string.Empty, MockMoney);
+
+            var result = subject.Validate();
+
+            Assert.Single(result);
+            Assert.Collection<IRule>(result, 
+                item => Assert.Equal(InvoiceLine.ValidationRules.ProductName, item)
+            );
+        }
+
+        [Fact]
+        public void Should_ReturnBrokenRule_When_MoneyIsNull_When_Validate()
+        {
+            var subject = new InvoiceLine(ProductName, null);
+
+            var result = subject.Validate();
+
+            Assert.Single(result);
+            Assert.Collection<IRule>(result, 
+                item => Assert.Equal(InvoiceLine.ValidationRules.Money, item)
+            );
         }
     }
 }
